c23596d98fa4429fd00e4f23ad863f07ba7f5f6c
Grace <grko@ucsc.edu>
2019-01-14 19:33:55 -0800

    add DungeonScene
    > changed .gitignore file to exclude sln
    + added DungeonScene
    + added CombatManager

---
+++ Spellbook/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+// script to manage combat in DungeonScene
+public class CombatManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button Button_ok;
+
+    [SerializeField]
+    private GameObject Panel_starting;
+
+    [SerializeField]
+    private GameObject Panel_choices;
+
+    public void onOKClick()
+    {
+        Panel_choices.SetActive(true);
+        Panel_starting.SetActive(false);
+    }
+}
