2e18e237c6930fafba954c8044b592743b07fb56
Grace <grko@ucsc.edu>
2019-01-15 16:55:21 -0800

    add functions to inventory and help button

--- Spellbook/Assets/Scripts/CombatManager.cs
+++ Spellbook/Assets/Scripts/CombatManager.cs
@@ -7,6 +7,7 @@ using System;
 // script to manage combat in DungeonScene
 public class CombatManager : MonoBehaviour
 {
+    // serializefield private variables
     [SerializeField]
     private Button Button_ok;
 
@@ -16,9 +17,49 @@ public class CombatManager : MonoBehaviour
     [SerializeField]
     private GameObject Panel_choices;
 
-    public void onOKClick()
+    [SerializeField]
+    private GameObject Panel_inventory;
+
+    [SerializeField]
+    private GameObject Panel_help;
+
+    // private variables
+    private bool bInventoryOpen = false;
+    private bool bHelpOpen = false;
+
+    // when ok button is clicked
+    public void okClick()
     {
         Panel_choices.SetActive(true);
         Panel_starting.SetActive(false);
+    }
+
+    // when inventory button is clicked
+    public void inventoryClick()
+    {
+        if (bInventoryOpen == false)
+        {
+            Panel_inventory.SetActive(true);
+            bInventoryOpen = true;
+        }
+        else if (bInventoryOpen == true)
+        {
+            Panel_inventory.SetActive(false);
+            bInventoryOpen = false;
+        }
+    }
+
+    public void helpClick()
+    {
+        if (bHelpOpen == false)
+        {
+            Panel_help.SetActive(true);
+            bHelpOpen = true;
+        }
+        else if (bHelpOpen == true)
+        {
+            Panel_help.SetActive(false);
+            bHelpOpen = false;
+        }
     }
 }
