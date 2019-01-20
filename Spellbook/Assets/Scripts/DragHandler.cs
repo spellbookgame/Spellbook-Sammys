fc51db528ade29c04dd4f99ff368890adf804a4c
Grace <grko@ucsc.edu>
2019-01-20 13:59:52 -0800

    not currently working but something started for spell casting screen

---
+++ Spellbook/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// code by Kiwasi Games
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject itemToDrag;
+    private Vector3 startPos;
+    private Transform startParent;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPos = transform.position;
+        startParent = transform.parent;
+        // allows to pass events through the item being dragged
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //itemToDrag = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        // checking to see if the item is being dragged into a new slot
+        if (transform.parent == startParent)
+        {
+            transform.position = startPos;
+        }
+        
+    }
+}
---
+++ Spellbook/Assets/Scripts/SlotHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SlotHandler : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            // if it has a child, return the first child
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    GameObject g;
+    DragHandler dragHandler;
+
+    void Start()
+    {
+        g = GameObject.FindGameObjectWithTag("Spell Piece");
+        dragHandler = g.GetComponent<DragHandler>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // if the slot has no item, then allow item to be dragged in
+        if(!item)
+        {
+            dragHandler.itemToDrag.transform.SetParent(transform);
+        }
+    }
+}
