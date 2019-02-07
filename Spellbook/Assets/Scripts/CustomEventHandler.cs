9a89431db93afd982d8a5416581348de503af8de
Grace <grko@ucsc.edu>
2019-02-07 13:26:00 -0800

    add scan delay

--- Spellbook/Assets/Scripts/CustomEventHandler.cs
+++ Spellbook/Assets/Scripts/CustomEventHandler.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using Vuforia;
 
 public class CustomEventHandler : MonoBehaviour, ITrackableEventHandler
@@ -58,42 +56,7 @@ public class CustomEventHandler : MonoBehaviour, ITrackableEventHandler
 
     protected virtual void OnTrackingFound()
     {
-        if (mTrackableBehaviour.TrackableName.Equals("decal_chronomancer"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Chronomancer scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
-        else if (mTrackableBehaviour.TrackableName.Equals("decal_summoner"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Summoner scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
-        else if (mTrackableBehaviour.TrackableName.Equals("decal_alchemist"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Alchemist scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
-        else if (mTrackableBehaviour.TrackableName.Equals("decal_arcanist"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Arcanist scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
-        else if (mTrackableBehaviour.TrackableName.Equals("decal_illusionist"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Trickster scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
-        else if (mTrackableBehaviour.TrackableName.Equals("decal_elementalist"))
-        {
-            panel.transform.GetChild(0).GetComponent<Text>().text = "Elementalist scanned";
-            panel.SetActive(true);
-            panelOpen = true;
-        }
+        StartCoroutine(ScanTime(mTrackableBehaviour.TrackableName));
     }
 
     protected virtual void OnTrackingLost()
@@ -101,6 +64,48 @@ public class CustomEventHandler : MonoBehaviour, ITrackableEventHandler
         if(panelOpen)
         {
             panel.SetActive(false);
+        }
+    }
+
+    IEnumerator ScanTime(string target_name)
+    {
+        yield return new WaitForSeconds(3);
+
+        if (target_name.Equals("decal_chronomancer"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Chronomancer scanned";
+            panel.SetActive(true);
+            panelOpen = true;
+        }
+        else if (target_name.Equals("decal_summoner"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Summoner scanned";
+            panel.SetActive(true);
+            panelOpen = true;
+        }
+        else if (target_name.Equals("decal_alchemist"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Alchemist scanned";
+            panel.SetActive(true);
+            panelOpen = true;
+        }
+        else if (target_name.Equals("decal_arcanist"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Arcanist scanned";
+            panel.SetActive(true);
+            panelOpen = true;
+        }
+        else if (target_name.Equals("decal_illusionist"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Trickster scanned";
+            panel.SetActive(true);
+            panelOpen = true;
+        }
+        else if (target_name.Equals("decal_elementalist"))
+        {
+            panel.transform.GetChild(0).GetComponent<Text>().text = "Elementalist scanned";
+            panel.SetActive(true);
+            panelOpen = true;
         }
     }
 }
