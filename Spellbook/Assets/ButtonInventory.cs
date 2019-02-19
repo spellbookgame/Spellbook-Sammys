0e18dc9757252870f47b8b03f555a152ef351bbe
moicompsci <moi.compsci@gmail.com>
2019-02-19 09:09:18 -0800

    Integrated Jans inventory into verticle slice

---
+++ Spellbook/Assets/ButtonInventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInventory : MonoBehaviour
+{
+    bool isShowing = false;
+    public GameObject inventory;
+    public void onClickInventory()
+    {
+        if (isShowing)
+        {
+            isShowing = false;
+            inventory.SetActive(false);
+        }
+        else
+        {
+            isShowing = true;
+            inventory.SetActive(true);
+        }
+    }
+}
---
+++ Spellbook/Assets/Scripts/ItemTestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTestScript : MonoBehaviour
+{
+    public Item item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() 
+    {
+        Inventory.instance.Add(item);
+        Debug.Log("Testing " + item.name);
+        Destroy(this.gameObject);
+    }
+}
