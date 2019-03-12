0dd6a563a4758ca805366188b5e20706273bd8f3
Grace <grko@ucsc.edu>
2019-03-12 16:41:33 -0700

    add new dice roll script with spell/quest tracker implemented

--- Spellbook/Assets/_Scripts/DiceRoll.cs
+++ Spellbook/Assets/_Scripts/DiceRoll.cs
@@ -1,49 +1,115 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEditor;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Controller for the MagicDice prefab.
+/// 
+/// Written by Malcolm Riley
+/// CMPS 17X, Winter 2019
+/// </summary>
 public class DiceRoll : MonoBehaviour
 {
-    [SerializeField] private GameObject TextBox;
+
+    // Public fields
+    public SpriteRenderer dicePips;
+    public SpriteRenderer diceCube;
+    public Sprite pipsNine;
+    public Sprite pipsEight;
+    public Sprite pipsSeven;
+    public Sprite pipsSix;
+    public Sprite pipsFive;
+    public Sprite pipsFour;
+    public Sprite pipsThree;
+    public Sprite pipsTwo;
+    public Sprite pipsOne;
+
+    [Range(0, 9)]
+    public int maxRoll = 6;
+
+    public int LastRoll
+    {
+        get { return _roll; }
+        private set { SetRoll(value); }
+    }
+
+    // private fields
+    private int _roll;
+    private Sprite[] _pipsArray;
+    private int _rollMinimum;
+    private int _rollMaximum;
+    private int _rollAdd;
+    private float _rollMult;
+    
+    // Grace Ko's additions: implementing spell/quest tracking and scanner
     [SerializeField] private Button rollButton;
+    [SerializeField] private GameObject diceTrayPanel;
 
     private int diceRoll;
     private int pressedNum;
     Player localPlayer;
 
-    private void Start()
+    void Start()
     {
+        _pipsArray = new Sprite[] { pipsOne, pipsTwo, pipsThree, pipsFour, pipsFive, pipsSix, pipsSeven, pipsEight, pipsNine };
+        SetDefaults();
+        // Roll();
+
         localPlayer = GameObject.FindGameObjectWithTag("LocalPlayer").GetComponent<Player>();
 
-        pressedNum = 0;
-        rollButton.onClick.AddListener(() => NumberGenerator());
+        pressedNum = 0; // to set button text to either roll or scan
+        rollButton.onClick.AddListener(() => Roll());
     }
 
-    public void NumberGenerator() 
+    public void Roll()
     {
-        // when button is clicked for first time, generate random number and change button to Scan
-        if(pressedNum == 0)
+        // when button is clicked for first time, roll and change button to Scan
+        if (pressedNum == 0)
         {
             string spellActive = SpellTracker.instance.CheckMoveSpell();
             if (spellActive.Equals("Accelerate"))
-                diceRoll = Random.Range(5, 7);
-            else
-                diceRoll = Random.Range(1, 7);
+            {
+                _rollMinimum = 5;
+                _rollMaximum = 9;
+            }
 
-            TextBox.GetComponent<Text>().text = diceRoll.ToString();
+            LastRoll = Clamp((int)(_rollMult * Random.Range(_rollMinimum, _rollMaximum + 1) + _rollAdd), _rollMinimum, _rollMaximum);
+            SetDefaults();
 
-            localPlayer.Spellcaster.spacesTraveled += diceRoll;
+            localPlayer.Spellcaster.spacesTraveled += LastRoll;
             QuestTracker.instance.CheckMoveQuest(diceRoll);
 
             rollButton.GetComponentInChildren<Text>().text = "Scan!";
             ++pressedNum;
         }
-        else if(pressedNum >= 1)
+        else if (pressedNum >= 1)
         {
-            gameObject.SetActive(false);
+            diceTrayPanel.SetActive(false);
             SceneManager.LoadScene("VuforiaScene");
         }
+    }
+
+    // Internal Methods
+    private void SetRoll(int value)
+    {
+        _roll = value;
+        dicePips.sprite = (value <= 0) ? null : _pipsArray[Clamp(_roll - 1, 0, _pipsArray.Length - 1)];
+    }
+
+    private void SetDefaults()
+    {
+        _rollMinimum = 1;
+        _rollMaximum = maxRoll;
+        _rollAdd = 0;
+        _rollMult = 1.0F;
+    }
+
+    // Why doesn't System.Math have an int clamp function, like seriously
+    private static int Clamp(int value, int min, int max)
+    {
+        if (value > max) return max;
+        if (value < min) return min;
+        return value;
     }
 }
