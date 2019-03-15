5f0264b52f1ae646682c9846f8d6be6a3bd4af79
moicompsci <moi.compsci@gmail.com>
2019-03-15 06:00:32 -0700

    Lobby Rework

---
+++ Spellbook/Assets/UI/Scripts/RandomizedAnimationStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RandomizedAnimationStart : MonoBehaviour {
+
+    // Public Fields
+    public string animationName;
+
+    void Start() {
+        GetComponent<Animator>().Play(animationName, -1, Random.Range(0.0F, 1.0F));
+    }
+}
