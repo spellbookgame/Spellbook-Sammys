be00408d588ad8e1cf0fcd46ebccd59cb68bb7ad
Grace <grko@ucsc.edu>
2019-04-17 20:46:16 -0700

    Implement Playwright

--- Spellbook/Assets/_Scripts/Spells/IllusionSpells/Playwright.cs
+++ Spellbook/Assets/_Scripts/Spells/IllusionSpells/Playwright.cs
@@ -7,13 +7,11 @@ public class Playwright : Spell
     public Playwright()
     {
         iTier = 1;
-        iManaCost = 1600;
-        iCoolDown = 3;
+        iManaCost = 200;
 
         sSpellName = "Playwright";
         sSpellClass = "Trickster";
-        sSpellInfo = "Destroy one of your puppets and upgrade two glyphs into their next highest tier. Can cast on an ally."
-                        + "Cannot cast if you do not have any active puppets.";
+        sSpellInfo = "Destroy a random item to change two of your runes into any runes of your choice.";
 
         requiredGlyphs.Add("Illusion A Glyph", 1);
         requiredGlyphs.Add("Illusion B Glyph", 1);
@@ -22,9 +20,35 @@ public class Playwright : Spell
 
     public override void SpellCast(SpellCaster player)
     {
-        // subtract mana and glyph costs
-        player.iMana -= iManaCost;
-           
-        PanelHolder.instance.displayNotify("You cast " + sSpellName, "", "OK");
+        if (player.iMana < iManaCost)
+        {
+            PanelHolder.instance.displayNotify("Not enough Mana!", "You do not have enough mana to cast this spell.", "OK");
+        }
+        if(player.inventory.Count <= 0)
+        {
+            PanelHolder.instance.displayNotify("No Items!", "You have no items to sacrifice to cast this spell.", "OK");
+        }
+        else
+        {
+            // subtract mana
+            player.iMana -= iManaCost;
+
+            string itemName;
+            // take a random index from the inventory of items and remove it 
+            List<ItemObject> playerInventory = new List<ItemObject>(player.inventory);
+            if(playerInventory.Count <= 1)
+            {
+                itemName = playerInventory[0].name;
+                player.RemoveFromInventory(playerInventory[0]);
+            }
+            else
+            {
+                int rIndex = (int)UnityEngine.Random.Range(0, playerInventory.Count);
+                itemName = playerInventory[rIndex].name;
+                player.RemoveFromInventory(playerInventory[rIndex]);
+            }
+
+            PanelHolder.instance.displayNotify(sSpellName, "You destroyed " +  itemName + ". Discard 2 of your runes and choose 2 runes of your choice from the deck.", "OK");
+        }
     }
 }
