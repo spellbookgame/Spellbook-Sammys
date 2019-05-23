11aa2bad6f4641ada37a47010bd1916790fbdc02
moicompsci <moi.compsci@gmail.com>
2019-05-22 17:13:58 -0700

    Crisis backend/networking

---
+++ Spellbook/Assets/_Scripts/Spells/AllCrisisDict.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllCrisisDict 
+{
+    public static readonly Dictionary<string, Action> CallCrisis = new Dictionary<string, Action>
+    {
+        {"Tsunami", CrisisHandler.instance.CallTsunami},
+        {"Plague", CrisisHandler.instance.CallPlague },
+        {"Boss Battle", CrisisHandler.instance.CallBossBattle }
+    };
+
+
+    public static readonly Dictionary<string, Action> FinishCrisis = new Dictionary<string, Action>
+    {
+        {"Tsunami", CrisisHandler.instance.FinishTsunami},
+        {"Plague", CrisisHandler.instance.FinishPlague },
+        {"Boss Battle", CrisisHandler.instance.FinishBossBattle }
+    };
+}
